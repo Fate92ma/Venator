c45124b529ed952e83a8f8cd71adf4c1a4986618
fati92ma <fatima131khaled@gmail.com>
2021-02-17 00:12:55 +0200

    Template

---
+++ js/script-AR.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+
+    // Search Modal
+
+    $(".closeMe").css("right", $(".searchMe input").innerHeight());
+
+});
---
+++ js/script.js
@@ -0,0 +1,162 @@
+$(document).ready(function () {
+
+    // Preloader
+
+    $('.preloader').fadeOut(4000);
+
+    // ScrollSpy
+
+    $('.section').headerhighlight({
+        elementsChanging: $('.nav-link'),
+        currentClass: 'nav-active',
+        offset: 0
+    });
+
+    // Sticky Header
+
+    $(window).scroll(function () {
+
+        var windScro = $(window).scrollTop();
+
+        if (windScro >= $(".pre-header").outerHeight()) {
+            $("#home").css("paddingTop", $("header").innerHeight());
+            $("header").addClass("fixed");
+            $(".logo-img").css("visibility", "visible");
+            $(".top i").css("visibility", "visible");
+
+        } else {
+            $("#home").css("padding-top", "0");
+            $("header").removeClass("fixed");
+            $(".logo-img").css("visibility", "hidden");
+            $(".top i").css("visibility", "hidden");
+        }
+    });
+
+    // Search Modal
+
+    $(".search i").on("click", function () {
+        $(".searchMe").css("visibility", "visible")
+    });
+
+    $(".closeMe").css("left", $(".searchMe input").innerHeight());
+    $(".closeMe").css("top", $(".searchMe input").innerHeight() + 15);
+
+    $(".closeMe").on("click", function () {
+        $(".searchMe").css("visibility", "hidden")
+    });
+
+    // Smooth Scroll
+
+    $('.list li a').click(function () {
+
+        var sectionTo = $(this).attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(sectionTo).offset().top
+        }, 1200);
+
+    });
+
+    // Home Carousel
+
+    var swiper = new Swiper('.swiper-container', {
+        autoplay: true,
+        interval: 800,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        }
+    });
+
+    // Numbers
+
+    $(window).scroll(function () {
+
+        if ($(this).scrollTop() >= 1375) {
+
+            $('.timer').countTo({
+                onUpdate: true
+            });
+        }
+    });
+
+    // Features
+
+    $('#mixedSlider').multislider({
+        duration: 1000,
+        interval: 2000
+    });
+
+    // Contact
+
+    var placeAttr = "  "
+
+    $("[placeholder]").focus(function () {
+
+        placeAttr = $(this).attr("placeholder");
+
+        $(this).attr("placeholder", " ");
+
+    }).blur(function () {
+
+        $(this).attr("placeholder", placeAttr);
+
+    });
+
+    // Back to top
+
+    $('.top i').on('click', function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1200);
+    });
+
+    // Language
+
+    $(".language i").on('click', function () {
+        $(".language").fadeOut()
+    });
+
+    $(".language i").mouseenter(function () {
+        $(".tooltiptext").fadeIn()
+    });
+
+    $(".language i").mouseleave(function () {
+        $(".tooltiptext").fadeOut()
+    });
+
+});
+
+// Testimonials
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
